d23b2453115895ca30cfbc4ec1f34d7c93026204
Arika <arif.quliev39@gmail.com>
2025-11-04 02:10:56 +0400

    first commit

---
+++ src/main/java/com/srms/SupportMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.srms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srms/SupportMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
